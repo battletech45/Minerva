636cf5ad2010aaaef453a3032d70372cc3ea6e84
Altay Taneri <taneri862@gmail.com>
2023-02-09 22:14:24 +0300

    Firebase project supoort(IOS, Android and Web) added to project and fixed gradle proble,

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:minerva/firebase_options.dart';
 import 'HomePage.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(MyApp());
 }
 
