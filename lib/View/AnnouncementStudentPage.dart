9d1a3f244584481b08b440660dccbecfc4a6e9d6
Altay Taneri <taneri862@gmail.com>
2023-02-15 21:12:13 +0300

    Revert "Revert "Custom drawer added to pages""
    This reverts commit d8870a481a303b16e1d66e7dc2e8dfeef5191b47.

--- lib/View/AnnouncementStudentPage.dart
+++ lib/View/AnnouncementStudentPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minerva/Model/CustomWidgets.dart';
 
 class AnnouncementStudentPage extends StatefulWidget {
   @override
@@ -201,52 +202,7 @@ class _AnnouncementStudentPageState extends State<AnnouncementStudentPage> {
           ],
         ),
       ),
-      drawer: Drawer(
-        width: 300,
-        backgroundColor: Colors.green,
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.person, size: 50.0),
-                  Text('Metin Baybars Arslan', style: TextStyle(color: Colors.white,fontSize: 20))
-                ],
-              ),
-            ),
-            ListTile(
-              title: Text('HOME',style: TextStyle(fontSize: 24),textAlign: TextAlign.center,),
-              onTap: (){},
-
-            ),
-            ListTile(
-              title: Text('PROFILE',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-              onTap: (){},
-            ),
-            ListTile(
-              title: Text('GRADES',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-              onTap: (){},
-            ),
-            ListTile(
-              title: Text('ATTENDANCE',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-              onTap: (){},
-            ),
-            ListTile(
-              title: Text('CHAT',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-              onTap: (){},
-            ),
-            ListTile(
-              title: Text('ANNOUNCEMENTS',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-              onTap: (){},
-            ),
-            ListTile(
-              title: Text('CONTENT',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-              onTap: (){},
-            ),
-          ],
-        ),
-      ),
+      drawer: customDrawer()
     );
   }
 }
