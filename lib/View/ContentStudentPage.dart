d162668b13804d224341c681da04a2e4c56158b7
Altay Taneri <taneri862@gmail.com>
2023-02-15 19:43:30 +0300

    Gesture Detector removed and listview physics added

--- lib/View/ContentStudentPage.dart
+++ lib/View/ContentStudentPage.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-class ContentStudent extends StatefulWidget {
-  const ContentStudent({super.key});
 
+class ContentStudent extends StatefulWidget {
   @override
   State<ContentStudent> createState() => _ContentStudentState();
 }
@@ -18,162 +17,108 @@ class _ContentStudentState extends State<ContentStudent> {
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Column(
+        child: ListView(
+          shrinkWrap: true,
+          physics: BouncingScrollPhysics(),
           children: [
             Text("MATERIALS",textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-            SizedBox(
-              height: 15,
+            style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold)),
+            SizedBox(height: 15.0),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+               child: ListTile(
+                 leading: Icon(Icons.class_),
+                 title: Text("Math", textAlign: TextAlign.center, style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+                 onTap: () {},
+                  ),
             ),
-            Expanded(
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                    GestureDetector(
-                      onTap: () {
-                       
-                      },
-                      child: Card(
-                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-                         child: ListTile(
-                          leading: Icon(Icons.class_),
-                          title: Text(
-                                    "Math",
-                                    textAlign: TextAlign.center,      
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
-                                 ),           
-                            ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Card(
-                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-                         child: ListTile(
-                          leading: Icon(Icons.class_),
-                          title: Text(
-                                    "Chemistry",
-                                    textAlign: TextAlign.center,      
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
-                                 ),           
-                            ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Card(
-                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-                         child: ListTile(
-                          leading: Icon(Icons.class_),
-                          title: Text(
-                                    "Physics",
-                                    textAlign: TextAlign.center,      
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
-                                 ),           
-                            ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Card(
-                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-                         child: ListTile(
-                          leading: Icon(Icons.class_),
-                          title: Text(
-                                    "History",
-                                    textAlign: TextAlign.center,      
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
-                                 ),           
-                            ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Card(
-                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-                         child: ListTile(
-                          leading: Icon(Icons.class_),
-                          title: Text(
-                                    "Biology",
-                                    textAlign: TextAlign.center,      
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
-                                 ),           
-                            ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Card(
-                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-                         child: ListTile(
-                          leading: Icon(Icons.class_),
-                          title: Text(
-                                    "Turkish",
-                                    textAlign: TextAlign.center,      
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
-                                 ),           
-                            ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Card(
-                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-                         child: ListTile(
-                          leading: Icon(Icons.class_),
-                          title: Text(
-                                    "English",
-                                    textAlign: TextAlign.center,      
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
-                                 ),           
-                            ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Card(
-                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-                         child: ListTile(
-                          leading: Icon(Icons.class_),
-                          title: Text(
-                                    "German",
-                                    textAlign: TextAlign.center,      
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
-                                 ),           
-                            ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        
-                      },
-                      child: Card(
-                          margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
-                         child: ListTile(
-                          leading: Icon(Icons.class_),
-                          title: Text(
-                                    "Literature",
-                                    textAlign: TextAlign.center,      
-                                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
-                                 ),           
-                            ),
-                      ),
-                    ),
-              ]),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+               child: ListTile(
+                leading: Icon(Icons.class_),
+                title: Text(
+                          "Chemistry",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
+                       ),
+                  ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+               child: ListTile(
+                leading: Icon(Icons.class_),
+                title: Text(
+                          "Physics",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
+                       ),
+                  ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+               child: ListTile(
+                leading: Icon(Icons.class_),
+                title: Text(
+                          "History",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
+                       ),
+                  ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+               child: ListTile(
+                leading: Icon(Icons.class_),
+                title: Text(
+                          "Biology",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
+                       ),
+                  ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+               child: ListTile(
+                leading: Icon(Icons.class_),
+                title: Text(
+                          "Turkish",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
+                       ),
+                  ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+               child: ListTile(
+                leading: Icon(Icons.class_),
+                title: Text(
+                          "English",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
+                       ),
+                  ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+               child: ListTile(
+                leading: Icon(Icons.class_),
+                title: Text(
+                          "German",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
+                       ),
+                  ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(horizontal: 18,vertical: 10),
+               child: ListTile(
+                leading: Icon(Icons.class_),
+                title: Text(
+                          "Literature",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500,),
+                       ),
+                  ),
             ),
           ],
         ),
@@ -227,6 +172,7 @@ class _ContentStudentState extends State<ContentStudent> {
     );
   }
 }
+// BU NEDIR !
 class Books extends StatelessWidget {
   const Books({super.key});
 
