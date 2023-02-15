d8870a481a303b16e1d66e7dc2e8dfeef5191b47
Altay Taneri <taneri862@gmail.com>
2023-02-15 21:11:12 +0300

    Revert "Custom drawer added to pages"
    This reverts commit 8150eb3a05e1a1448507703134b37b657c52726e.

--- lib/View/AttendanceStudentPage.dart
+++ lib/View/AttendanceStudentPage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:minerva/Model/CustomWidgets.dart';
 
 class AttendanceStudentPage extends StatefulWidget {
   @override
@@ -28,19 +27,223 @@ class _AttendanceStudentPageState extends State<AttendanceStudentPage> {
           children: <Widget>[
             Text('Attendance', textAlign: TextAlign.center, style: TextStyle(fontSize: 25.0, fontStyle: FontStyle.italic, fontWeight: FontWeight.bold)),
             SizedBox(height: 30.0),
-            customAttendanceCard('Teacher Tom'),
-            customAttendanceCard('Teacher Tom'),
-            customAttendanceCard('Teacher Tom'),
-            customAttendanceCard('Teacher Tom'),
-            customAttendanceCard('Teacher Tom'),
-            customAttendanceCard('Teacher Tom'),
-            customAttendanceCard('Teacher Tom'),
-            customAttendanceCard('Teacher Tom'),
-            customAttendanceCard('Teacher Tom'),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              color: Colors.deepOrange[400],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('Teacher Tom', textAlign: TextAlign.center),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('Course Name', style: TextStyle(backgroundColor: Colors.deepOrange[500])),
+                      Text('Attendance', style: TextStyle(backgroundColor: Colors.deepOrange[500]))
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('01/03/2023', style: TextStyle(backgroundColor: Colors.grey)),
+                      Text('2 Hours', style: TextStyle(backgroundColor: Colors.grey))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              color: Colors.deepOrange[400],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('Teacher Tom', textAlign: TextAlign.center),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('Course Name', style: TextStyle(backgroundColor: Colors.deepOrange[500])),
+                      Text('Attendance', style: TextStyle(backgroundColor: Colors.deepOrange[500]))
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('01/03/2023', style: TextStyle(backgroundColor: Colors.grey)),
+                      Text('2 Hours', style: TextStyle(backgroundColor: Colors.grey))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              color: Colors.deepOrange[400],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('Teacher Tom', textAlign: TextAlign.center),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('Course Name', style: TextStyle(backgroundColor: Colors.deepOrange[500])),
+                      Text('Attendance', style: TextStyle(backgroundColor: Colors.deepOrange[500]))
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('01/03/2023', style: TextStyle(backgroundColor: Colors.grey)),
+                      Text('2 Hours', style: TextStyle(backgroundColor: Colors.grey))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              color: Colors.deepOrange[400],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('Teacher Tom', textAlign: TextAlign.center),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('Course Name', style: TextStyle(backgroundColor: Colors.deepOrange[500])),
+                      Text('Attendance', style: TextStyle(backgroundColor: Colors.deepOrange[500]))
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('01/03/2023', style: TextStyle(backgroundColor: Colors.grey)),
+                      Text('2 Hours', style: TextStyle(backgroundColor: Colors.grey))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              color: Colors.deepOrange[400],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('Teacher Tom', textAlign: TextAlign.center),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('Course Name', style: TextStyle(backgroundColor: Colors.deepOrange[500])),
+                      Text('Attendance', style: TextStyle(backgroundColor: Colors.deepOrange[500]))
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('01/03/2023', style: TextStyle(backgroundColor: Colors.grey)),
+                      Text('2 Hours', style: TextStyle(backgroundColor: Colors.grey))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              color: Colors.deepOrange[400],
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('Teacher Tom', textAlign: TextAlign.center),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('Course Name', style: TextStyle(backgroundColor: Colors.deepOrange[500])),
+                      Text('Attendance', style: TextStyle(backgroundColor: Colors.deepOrange[500]))
+                    ],
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('01/03/2023', style: TextStyle(backgroundColor: Colors.grey)),
+                      Text('2 Hours', style: TextStyle(backgroundColor: Colors.grey))
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
-      drawer: customDrawer()
+      drawer: Drawer(
+       width: 300,
+       backgroundColor: Colors.green,
+       child: ListView(
+         children: <Widget>[
+           DrawerHeader(
+             child: Column(
+               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+               children: [
+                 Icon(Icons.person, size: 50.0),
+                 Text('Metin Baybars Arslan', style: TextStyle(color: Colors.white,fontSize: 20))
+               ],
+             ),
+           ),
+           ListTile(
+             title: Text('HOME',style: TextStyle(fontSize: 24),textAlign: TextAlign.center,),
+             onTap: (){},
+
+           ),
+           ListTile(
+             title: Text('PROFILE',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
+             onTap: (){},
+           ),
+           ListTile(
+             title: Text('GRADES',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
+             onTap: (){},
+           ),
+           ListTile(
+             title: Text('ATTENDANCE',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
+             onTap: (){},
+           ),
+           ListTile(
+             title: Text('CHAT',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
+             onTap: (){},
+           ),
+           ListTile(
+             title: Text('ANNOUNCEMENTS',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
+             onTap: (){},
+           ),
+           ListTile(
+             title: Text('CONTENT',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
+             onTap: (){},
+           ),
+         ],
+       ),
+     ),
    );
   }
 }
