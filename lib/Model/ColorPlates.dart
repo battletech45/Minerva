36898fa14faa5d0142d25d0cc1c4a30f6dce16a3
Altay Taneri <taneri862@gmail.com>
2023-02-24 21:53:19 +0300

    Added 2 new colors

--- lib/Model/ColorPlates.dart
+++ lib/Model/ColorPlates.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 class PageColors{
   static final  Color mainColor = Color.fromRGBO(28, 88, 140, 1);
+  static final  Color secondaryColor = Color.fromRGBO(165, 187, 210, 1);
+  static final  Color thirdColor = Color.fromRGBO(100, 136, 174, 1);
 }
