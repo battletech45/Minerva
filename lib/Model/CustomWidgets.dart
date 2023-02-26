f2b5cb12eaf0a5e6215c1a4daf707daef0dc0b8b
Metin Baybars Arslan <baybarsfb2000@hotmail.com>
2023-02-26 15:11:40 +0300

    Drawer Updated

--- lib/Model/CustomWidgets.dart
+++ lib/Model/CustomWidgets.dart
@@ -17,7 +17,6 @@ class customDrawer extends StatefulWidget {
 }
 
 class _customDrawerState extends State<customDrawer> {
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -26,60 +25,87 @@ class _customDrawerState extends State<customDrawer> {
       child: ListView(
         children: <Widget>[
           DrawerHeader(
-            padding: EdgeInsets.all(0),
-            child: Container(
+            decoration: BoxDecoration(
               color: PageColors.thirdColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.person, size: 50.0),
-                  Text('Metin Baybars Arslan', style: TextStyle(color: Colors.black,fontSize: 20))
-                ],
-              ),
+              borderRadius: BorderRadius.only(bottomRight: Radius.circular(30)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.person, size: 50.0),
+                Text('Metin Baybars Arslan',
+                    style: TextStyle(color: Colors.black, fontSize: 20))
+              ],
             ),
           ),
           ListTile(
-            title: Text('HOME',style: TextStyle(fontSize: 24),textAlign: TextAlign.center,),
-            onTap: (){
-               Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> HomePage()));
-            },
-
-          ),
-          ListTile(
-            title: Text('PROFILE',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-            onTap: (){
-              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> ProfilePage()));
-            },
-          ),
-          ListTile(
-            title: Text('GRADES',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-            onTap: (){
-              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> StudentGradePage()));
-            },
-          ),
-          ListTile(
-            title: Text('ATTENDANCE',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-            onTap: (){
-               Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> AttendanceTeacherPage()));
-            },
-          ),
-          ListTile(
-            title: Text('CHAT',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-            onTap: (){
-              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> ChatPage()));
-            },
-          ),
-          ListTile(
-            title: Text('ANNOUNCEMENTS',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
-            onTap: (){
-              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> AnnouncementTeacherPage()));
-            },
-          ),
-          ListTile(
-            title: Text('CONTENT',style: TextStyle(fontSize: 24),textAlign: TextAlign.center),
+            leading: Icon(Icons.house, size: 30,color: Colors.black54),
+            title: Text('HOME',
+                style: TextStyle(fontSize: 24), textAlign: TextAlign.justify),
             onTap: () {
-              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> ContentStudent()));
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => HomePage()));
             },
+          ),
+          ListTile(
+            leading: Icon(Icons.person_2, size: 30,color: Colors.black54),
+            title: Text('PROFILE',
+                style: TextStyle(fontSize: 24), textAlign: TextAlign.justify),
+            onTap: () {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => ProfilePage()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.grade_sharp, size: 30,color: Colors.black54),
+            title: Text('GRADES',
+                style: TextStyle(fontSize: 24), textAlign: TextAlign.justify),
+            onTap: () {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => StudentGradePage()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.fact_check_outlined, size: 30,color: Colors.black54),
+            title: Text('ATTENDANCE',
+                style: TextStyle(fontSize: 24), textAlign: TextAlign.justify),
+            onTap: () {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => AttendanceTeacherPage()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.chat, size: 30,color: Colors.black54),
+            title: Text('CHAT',
+                style: TextStyle(fontSize: 24), textAlign: TextAlign.justify),
+            onTap: () {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => ChatPage()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.announcement, size: 30,color: Colors.black54),
+            title: Text('ANNOUNCEMENTS',
+                style: TextStyle(fontSize: 24), textAlign: TextAlign.justify),
+            onTap: () {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (context) => AnnouncementTeacherPage()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.content_copy, size: 30,color: Colors.black54),
+            title: Text('CONTENT',
+                style: TextStyle(fontSize: 24), textAlign: TextAlign.justify),
+            onTap: () {
+              Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => ContentStudent()));
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.logout, size: 20,color: Colors.black54),
+            title: Text('SIGN OUT',
+                style: TextStyle(fontSize: 18), textAlign: TextAlign.justify),
+            onTap: () {},
           ),
         ],
       ),
@@ -88,7 +114,6 @@ class _customDrawerState extends State<customDrawer> {
 }
 
 class customAttendanceCard extends StatelessWidget {
-
   String teacherName;
 
   @override
@@ -107,17 +132,19 @@ class customAttendanceCard extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              Text('Course Name', style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.bold)),
-              Text('Attendance', style: TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.bold))
+              Text('Course Name',
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold)),
+              Text('Attendance',
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic, fontWeight: FontWeight.bold))
             ],
           ),
           SizedBox(height: 10.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text('01/03/2023'),
-              Text('2 Hours')
-            ],
+            children: <Widget>[Text('01/03/2023'), Text('2 Hours')],
           ),
           SizedBox(height: 10.0)
         ],
