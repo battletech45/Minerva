26a7f6f923cdea6a8b77383d602c33fc4e21bfc4
battletech45 <taneri862@gmail.com>
2023-03-09 15:52:28 +0300

    Print added to save functions

--- lib/Control/SharedFunctions.dart
+++ lib/Control/SharedFunctions.dart
@@ -8,17 +8,22 @@ class SharedFunctions {
   static String sharedPreferencesUserEmailKey = "USERMAILKEY";
 
   static Future<bool> saveUserLoggedInSharedPreference(bool isUserLoggedIn) async {
+    print(isUserLoggedIn);
     SharedPreferences preferences = await SharedPreferences.getInstance();
     return await preferences.setBool(sharedPreferencesUserLoggedInKey, isUserLoggedIn);
   }
   static Future<bool> saveUserNameSharedPreference(String userName) async {
+    print(userName);
     SharedPreferences preferences = await SharedPreferences.getInstance();
     return await preferences.setString(sharedPreferencesUserNameKey, userName);
   }
   static Future<bool> saveUserEmailSharedPreference(String userEmail) async {
+    print(userEmail);
     SharedPreferences preferences = await SharedPreferences.getInstance();
     return await preferences.setString(sharedPreferencesUserLoggedInKey, userEmail);
   }
+
+
   static Future<bool?> getUserLoggedInSharedPreference() async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     return await preferences.getBool(sharedPreferencesUserLoggedInKey);
