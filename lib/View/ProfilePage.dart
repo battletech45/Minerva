8f42feb9db42b5fdb03bbe59333670f4679a697d
Altay Taneri <taneri862@gmail.com>
2023-03-09 17:19:20 +0300

    Profile page alignment solved and code refactored

--- lib/View/ProfilePage.dart
+++ lib/View/ProfilePage.dart
@@ -28,41 +28,21 @@ class _ProfilePageState extends State<ProfilePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CircleAvatar(
-                  backgroundColor: Colors.black,
-                  radius: 55,
-                ),
+                CircleAvatar(backgroundColor: Colors.black, radius: 55.0, child: Icon(Icons.person, size: 50.0)),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Bertan Silleli",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(
-              height: 8,
-            ),
+            SizedBox(height: 10.0),
+            Text("Bertan Silleli", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+            SizedBox(height: 8.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "11-A",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                Text(
-                  "20180601036",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                ),
+              children: <Widget>[
+                Text("11-A", style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400)),
+                SizedBox(width: 15.0),
+                Text("20180601036", style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400)),
               ],
             ),
-            SizedBox(
-              height: 23,
-            ),
+            SizedBox(height: 23.0),
             Expanded(
               child: ListView(
                   shrinkWrap: true,
@@ -74,47 +54,36 @@ class _ProfilePageState extends State<ProfilePage> {
                         height: 65,
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Icon(Icons.person_2),
-                            Text(
-                              "Name Surname",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w500),
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(Icons.person_2),
+                                SizedBox(width: 20.0),
+                                Text("Name Surname", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                              ],
                             ),
-                            SizedBox(
-                              width: 14,
-                            ),
-                            Text(
-                              "Bertan Silleli",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w400),
-                            ),
+                            Text("Bertan Silleli", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
                           ],
                         ),
                       ),
                     ),
                     Card(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+                        margin: EdgeInsets.symmetric(horizontal: 18, vertical: 5),
                         child: Container(
                           height: 65,
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Icon(Icons.numbers_rounded),
-                              Text(
-                                "Student Number",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Icon(Icons.numbers_rounded),
+                                  SizedBox(width: 20.0),
+                                  Text("Student Number", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
+                                ],
                               ),
-                              SizedBox(
-                                width: 14,
-                              ),
-                              Text(
-                                "2010601001",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
-                              ),
+                              Text("2010601001", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
                             ],
                           ),
                         )),
@@ -125,15 +94,14 @@ class _ProfilePageState extends State<ProfilePage> {
                           height: 65,
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Icon(Icons.cabin_rounded),
-                              Text(
-                                "Class",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                width: 14,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Icon(Icons.cabin_rounded),
+                                  SizedBox(width: 20.0),
+                                  Text("Class",style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                                ],
                               ),
                               Text(
                                 "11-A",
@@ -150,15 +118,14 @@ class _ProfilePageState extends State<ProfilePage> {
                           height: 65,
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Icon(Icons.school_rounded),
-                              Text(
-                                "School",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                width: 14,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Icon(Icons.school_rounded),
+                                  SizedBox(width: 20.0),
+                                  Text("School", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                                ],
                               ),
                               Text(
                                 "Yunus Emre Anadolu Lisesi",
@@ -175,11 +142,14 @@ class _ProfilePageState extends State<ProfilePage> {
                           height: 65,
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Icon(Icons.email_rounded),
-                              Text(
-                                "Email",
-                                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Icon(Icons.email_rounded),
+                                  SizedBox(width: 20.0),
+                                  Text("Email",style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                                ],
                               ),
                               Text(
                                 "student@example.com",
@@ -188,57 +158,8 @@ class _ProfilePageState extends State<ProfilePage> {
                               ),
                             ],
                           ),
-                        )),
-                    Card(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 5),
-                        child: Container(
-                          height: 65,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Icon(Icons.password_rounded),
-                              Text(
-                                "Password",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                width: 14,
-                              ),
-                              Text(
-                                "*************",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
-                              ),
-                            ],
-                          ),
-                        )),
-                    Card(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 5),
-                        child: Container(
-                          height: 65,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Icon(Icons.password_rounded),
-                              Text(
-                                "Password",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                width: 14,
-                              ),
-                              Text(
-                                "*************",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
-                              ),
-                            ],
-                          ),
-                        )),
+                        )
+                    ),
                   ]),
             ),
           ]),
