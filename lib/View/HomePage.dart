323196c79e8b216a0dc3e028b0519f1aca321786
yarkinata <yarkinata6268@gmail.com>
2023-03-10 20:09:52 +0300

    Home page content feed template design implemented

--- lib/View/HomePage.dart
+++ lib/View/HomePage.dart
@@ -9,7 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
-
+  bool click=true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +21,107 @@ class _HomePage extends State<HomePage> {
           title: Text("HOME", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
         ),
         body: Center(
-          child: Column(
+          child: ListView(
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
             children: <Widget> [
+             // SizedBox(height: 20),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                    child: CircleAvatar(
+                      radius: 25,
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Text("Yarkın Ata",style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),)
+                ],
+              ),
+              
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  height: 300,
+                  width: 400,
+                  decoration: BoxDecoration(
+                     color: PageColors.secondaryColor,
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                ),
+              ),
+              Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 5),
+                      child:Icon(Icons.check,size: 38,)
+                    ),
+                  ],
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Padding(
+                   padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                    child: CircleAvatar(
+                      radius: 25,
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Text("Yarkın Ata",style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),)
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  height: 300,
+                  width: 400,
+                  decoration: BoxDecoration(
+                     color: PageColors.secondaryColor,
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                ),
+              ),
+               Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 5),
+                      child:Icon(Icons.check,size: 38,)
+                    ),
+                  ],
+              ),
+              SizedBox(height: 30),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                    child: CircleAvatar(
+                      radius: 25,
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Text("Yarkın Ata",style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold),)
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Container(
+                  height: 300,
+                  width: 400,
+                  decoration: BoxDecoration(
+                     color: PageColors.secondaryColor,
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                ),
+              ),
+               Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25,vertical: 5),
+                      child:Icon(Icons.check,size: 38,)
+                    ),
+                  ],
+              ),
             ],
           ),
         ),
