1e05f12338bc1a144392077040cf8ced21b69aba
battletech45 <taneri862@gmail.com>
2023-03-14 19:19:23 +0300

    Created basic chat functions (send and get)

--- lib/Control/FirebaseFunctions.dart
+++ lib/Control/FirebaseFunctions.dart
@@ -10,6 +10,7 @@ class FirebaseFunctions {
   final CollectionReference studentCollection = FirebaseFirestore.instance.collection('students');
   final CollectionReference teacherCollection = FirebaseFirestore.instance.collection('teachers');
   final CollectionReference schoolCollection = FirebaseFirestore.instance.collection('schools');
+  final CollectionReference chatsCollection = FirebaseFirestore.instance.collection('chats');
 
   Future createStudent(String studentName, String password, String email, String tc, String studentNumber) async {
     await studentCollection.add({
@@ -50,5 +51,32 @@ class FirebaseFunctions {
   Future<QuerySnapshot> getSchoolData(String email) async {
     QuerySnapshot snapshot = await schoolCollection.where('email', isEqualTo: email).get();
     return snapshot;
+  }
+  Future<Stream<QuerySnapshot>> getChats(String chatID) async {
+    return FirebaseFirestore.instance.collection('chats').doc(chatID).collection('messages').orderBy('time').snapshots();
+  }
+  void sendMessage(String chatID, chatMessageData) {
+    FirebaseFirestore.instance.collection('chats').doc(chatID).collection('messages').add(chatMessageData);
+    FirebaseFirestore.instance.collection('chats').doc(chatID).update({
+      'recentMessage': chatMessageData['message'],
+      'recentMessageSender': chatMessageData['sender'],
+      'recentMessageTime': chatMessageData['time'].toString()
+    });
+  }
+  Future createChat(String studentID, String studentName, String teacherID, String teacherName) async {
+    DocumentReference chatDocRef = await chatsCollection.add({
+      'members': [],
+      'chatID': '',
+      'recentMessage': '',
+      'recentMessageSender': '',
+    });
+
+    await chatDocRef.update({
+      'members': FieldValue.arrayUnion([studentID + '_' + studentName]),
+      'chatID': chatDocRef.id
+    });
+    await chatDocRef.update({
+      'members': FieldValue.arrayUnion([teacherID + '_' + teacherName])
+    });
   }
 }
