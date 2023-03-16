7b215eef732c5a11a4bbd5fb1652ce5770179125
battletech45 <taneri862@gmail.com>
2023-03-16 14:56:57 +0300

    ChatPage Firebase implementation done for first usage

--- lib/View/ChatPage.dart
+++ lib/View/ChatPage.dart
@@ -1,19 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:minerva/Control/FirebaseFunctions.dart';
 import 'package:minerva/Model/WidgetProperties.dart';
 import 'package:minerva/Model/CustomWidgets.dart';
 import '../Model/ChatTile.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  final String chatID;
+  final String userName;
+
+  const ChatPage({Key? key, required this.chatID, required this.userName}) : super(key: key);
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  List<ChatTile> _message = [];
+  Stream<QuerySnapshot>? _chats;
   TextEditingController _textEditingController = TextEditingController();
-  bool testSender = false;
+
+
+  _sendMessage() {
+    if(_textEditingController.text.isNotEmpty) {
+      Map<String, dynamic> chatMessageMap = {
+        "message": _textEditingController.text,
+        "sender": widget.userName,
+        "time": DateTime.now().millisecondsSinceEpoch
+      };
+      FirebaseFunctions().sendMessage(widget.chatID, chatMessageMap);
+      FirebaseFunctions().getChats(widget.chatID).then((Stream<QuerySnapshot> val) {
+        setState(() {
+          _chats = val;
+          _textEditingController.text = "";
+        });
+      });
+    }
+  }
+
+  Widget _chatMessages() {
+    return StreamBuilder <QuerySnapshot>(
+        stream: _chats,
+        builder: (context, snapshot) {
+          return snapshot.hasData ? ListView.builder(
+            physics: BouncingScrollPhysics(),
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+              return ChatTile(message: snapshot.data!.docs[snapshot.data!.docs.length - index - 1].get("message"), sender: snapshot.data!.docs[snapshot.data!.docs.length - index - 1].get("sender"), sentByMe: widget.userName == snapshot.data!.docs[snapshot.data!.docs.length - index - 1].get("sender"));
+              }
+          ) : Container();
+        },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseFunctions().getChats(widget.chatID).then((Stream<QuerySnapshot> val) {
+      setState(() {
+        _chats = val;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +78,7 @@ class _ChatPageState extends State<ChatPage> {
         child: Column(
           children: <Widget>[
             Expanded(
-              child: ListView.builder(
-                itemCount: _message.length,
-                itemBuilder: (context, index) {
-                  return _message[index];
-                },
-              ),
+              child: _chatMessages()
             ),
             Container(
               padding: EdgeInsets.symmetric(horizontal: 8),
@@ -49,6 +92,8 @@ class _ChatPageState extends State<ChatPage> {
                   ),
                   IconButton(icon: Icon(Icons.send),
                     onPressed: () {
+                      _sendMessage();
+                    }/*
                       if (_textEditingController.text.isNotEmpty) {
                         setState(() {
                           _message.add(ChatTile(message: _textEditingController.text, sentByMe: testSender, sender: 'Altay'),
@@ -56,8 +101,8 @@ class _ChatPageState extends State<ChatPage> {
                           _textEditingController.clear();
                           testSender =! testSender;
                         });
-                      }
-                    },)
+                      }*/
+                  )
                 ],
               ),
             ),
