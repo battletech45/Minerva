dfb83746fd9d6a60c76c0fc29578d4fd4e76474a
yarkinata <yarkinata6268@gmail.com>
2023-03-20 11:35:33 +0300

    Dropdown menu converted to checkboxes

--- lib/View/TeacherUploadHomework.dart
+++ lib/View/TeacherUploadHomework.dart
@@ -15,6 +15,17 @@ class UploadHomework extends StatefulWidget {
 class _UploadHomeworkState extends State<UploadHomework> {
   PlatformFile? pickedFile;
   UploadTask? uploadTask;
+  bool isSelected = false;
+  List<String> classes = [
+    "11-A",
+    "11-B",
+    "11-C",
+    "11-D",
+    "12-A",
+    "12-B",
+    "12-C",
+    "12-D",
+  ];
 
   Future selectFile() async {
     final result = await FilePicker.platform.pickFiles();
@@ -30,29 +41,16 @@ class _UploadHomeworkState extends State<UploadHomework> {
 
     final ref = FirebaseStorage.instance.ref().child(path);
     setState(() {
-      uploadTask =ref.putFile(file);
-
+      uploadTask = ref.putFile(file);
     });
     final snapshot = await uploadTask!.whenComplete(() {});
     final urlDownload = await snapshot.ref.getDownloadURL();
     print('Download Link: $urlDownload');
 
     setState(() {
-      uploadTask =null;
+      uploadTask = null;
     });
   }
-
-  List<String> classes = [
-    "11-A",
-    "11-B",
-    "11-C",
-    "11-D",
-    "12-A",
-    "12-B",
-    "12-C",
-    "12-D",
-  ];
-  String dropdownvalue = '11-A';
 
   @override
   Widget build(BuildContext context) {
@@ -74,48 +72,54 @@ class _UploadHomeworkState extends State<UploadHomework> {
               shrinkWrap: true,
               physics: BouncingScrollPhysics(),
               children: [
-                ListTile(
-                  title: Text("qweqwe"),
-                )
-              ],
-            ),
-            */
-        
-            Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-              children: [
                 Container(
-                  height: 50,
-                  width: 110,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: PageColors.secondaryColor,
-                  ),
-                  child: DropdownButton(
-                      isExpanded: true,
-                      alignment: AlignmentDirectional.center,
-                      borderRadius: BorderRadius.circular(10),
-                      dropdownColor: PageColors.secondaryColor,
-                      value: dropdownvalue,
-                      items: classes.map((String myClass) {
-                        return DropdownMenuItem(
-                          value: myClass,
-                          child: Center(
-                              child: Text(
-                            myClass,
-                            style: TextStyle(fontSize: 20),
-                          )),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          dropdownvalue = newValue!;
-                        });
-                      }),
-                ),
+                color: Colors.blue[100],
+                child: Text(pickedFile!.name),       ),
               ],
+            ), */
+            
+            //Birini seçince hepisi işaretleniyor!!!
+            Container(
+              height: 400,
+              margin: EdgeInsets.symmetric(horizontal:10),
+              child: SingleChildScrollView(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  childAspectRatio: 3,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10),
+                  shrinkWrap: true,
+                  physics: BouncingScrollPhysics(),
+                  itemCount: classes.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: PageColors.secondaryColor,
+                      ),
+                      child: CheckboxListTile(
+                          title: Text(
+                            classes[index],
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.black,
+                                letterSpacing: 0.5),
+                          ),
+                          value: isSelected,
+                          selected: !isSelected,
+                          onChanged: (bool? selectedValue) {
+                            setState(() {
+                              isSelected = selectedValue!;
+                            });
+                          }),
+                    );
+                  },
+                ),
+              ),
             ),
-
             SizedBox(
               height: 30,
             ),
@@ -150,7 +154,6 @@ class _UploadHomeworkState extends State<UploadHomework> {
                   onPressed: uploadFile,
                 ),
                 SizedBox(height: 32),
-
                 buildProgress(),
               ],
             ),
@@ -158,10 +161,11 @@ class _UploadHomeworkState extends State<UploadHomework> {
         ),
         drawer: customDrawer());
   }
+
   //Bu buildProgress çalışmıyo sonra bakalım
-  Widget buildProgress ()=> StreamBuilder<TaskSnapshot>(
+  Widget buildProgress() => StreamBuilder<TaskSnapshot>(
       stream: uploadTask?.snapshotEvents,
-      builder: (context,snapshot) {
+      builder: (context, snapshot) {
         if (snapshot.hasData) {
           final data = snapshot.data!;
           double progress = data.bytesTransferred / data.totalBytes;
@@ -176,21 +180,18 @@ class _UploadHomeworkState extends State<UploadHomework> {
                   color: Colors.green,
                 ),
                 Center(
-                  child: Text('${100 * progress.roundToDouble()} %',
-                    style: TextStyle(color: Colors.white),),
+                  child: Text(
+                    '${100 * progress.roundToDouble()} %',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ],
             ),
           );
+        } else {
+          return SizedBox(
+            height: 50,
+          );
         }
-        else {
-          return SizedBox(height: 50,);
-
-        }
-      }
-
-
-  );
-
+      });
 }
-
