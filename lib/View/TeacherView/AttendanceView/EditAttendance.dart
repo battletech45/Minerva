1bef70f3e0df1d1e285876a5405f5fb790956ff6
yarkinata <yarkinata6268@gmail.com>
2023-03-30 00:22:49 +0300

    Dropdown packed added and used

--- lib/View/TeacherView/AttendanceView/EditAttendance.dart
+++ lib/View/TeacherView/AttendanceView/EditAttendance.dart
@@ -1,3 +1,4 @@
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import '../../../Model/CustomWidgets.dart';
 import '../../../Model/WidgetProperties.dart';
@@ -22,6 +23,15 @@ class _EditAttendanceState extends State<EditAttendance> {
     "YAĞMUR",
     "ECE",
   ];
+ final List<String> classes = [
+  '11-A',
+  '10-D',
+  '12-A',
+  '11-B',
+  '12-C',
+];
+String? selectedValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,16 +43,61 @@ class _EditAttendanceState extends State<EditAttendance> {
         title: Text("Edit Attendance",
             style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
       ),
-      body: Container(
-        child: ListView.builder(
-          physics: BouncingScrollPhysics(),
-            itemCount: students.length,
-            itemBuilder: (context, index) {
-              return Container(
-                decoration: BoxDecoration(border:Border(bottom: BorderSide(color: PageColors.secondaryColor,width:2))),
-                child: customEditExpansionPanel(name: students[index])
-              );
-            }),
+      body: Column(
+        children: [
+          SizedBox(height: 15),
+          DropdownButtonHideUnderline(
+        child: DropdownButton2(
+          hint: Text(
+            'Select Class',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Theme.of(context).hintColor,
+            ),
+          ),
+          items: classes
+                  .map((item) => DropdownMenuItem<String>(
+            value: item,
+            child: Text(
+              item,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 20,
+                
+              ),
+            ),
+          ))
+                  .toList(),
+          value: selectedValue,
+          onChanged: (value) {
+            setState(() {
+              selectedValue = value as String;
+            });
+          },
+          buttonStyleData: const ButtonStyleData(
+            height: 40,
+            width: 200,
+          ),
+          menuItemStyleData: const MenuItemStyleData(
+            height: 50,
+          ),
+        ),
+      ),
+          Expanded(
+            child: Container(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                  itemCount: students.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      decoration: BoxDecoration(border:Border(bottom: BorderSide(color: PageColors.secondaryColor,width:2))),
+                      child: customEditExpansionPanel(name: students[index])
+                    );
+                  }),
+            ),
+          ),
+        ],
       ),
       drawer: customDrawer(),
     );
