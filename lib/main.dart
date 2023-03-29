65caec8a9a251c6d91af378941a275767d144fcb
Altay Taneri <taneri862@gmail.com>
2023-03-29 23:00:23 +0300

    Users can stay logged in

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:minerva/Control/SharedFunctions.dart';
+import 'package:minerva/View/StudentView/ProfileView/ProfilePage.dart';
+import 'package:minerva/View/TeacherView/ProfileView/ProfileTeacher.dart';
 import 'package:minerva/firebase_options.dart';
 import 'View/WelcomePage.dart';
 
@@ -11,12 +14,42 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool isLoggedIn = false;
+  bool isStudent = false;
+
+  _checkUserStatus() async {
+    var savedLoggedIn = await SharedFunctions.getUserLoggedInSharedPreference();
+    var userStudent = await SharedFunctions.getUserStudentSharedPreference();
+    if(savedLoggedIn != null && userStudent != null) {
+      setState(() {
+        isLoggedIn = savedLoggedIn;
+        isStudent = userStudent;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkUserStatus();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return isLoggedIn ? MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: isStudent ? ProfilePage() : ProfileTeacherPage()
+    ) :
+    MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
