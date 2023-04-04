4ce9ba5897688faf9a918fb6e9dd9d91e2901462
Altay Taneri <taneri862@gmail.com>
2023-04-04 17:18:25 +0300

    Example usage of customContentFeed

--- lib/View/HomePage.dart
+++ lib/View/HomePage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:minerva/Model/WidgetProperties.dart';
@@ -12,24 +13,40 @@ class HomePage extends StatefulWidget {
 
 class _HomePage extends State<HomePage> {
   bool click = false;
-  String userName="";
-  String email="";
+  String userName = "";
+  String email = "";
+  String link = '';
+  bool isLoading = false;
+  Image? photo;
 
   _getUserData() async {
     var val = await SharedFunctions.getUserEmailSharedPreference();
     setState(() {
       email = val!;
     });
-    print(email);
     var user = await FirebaseFunctions().getStudentData(email);
     setState(() {
       userName = user.docs[0].get('studentName');
      
     });
   }
-  @override void initState() {
-   _getUserData();
+  _getImaage() async {
+    setState(() {
+      isLoading = true;
+    });
+    final ref = await FirebaseStorage.instance.ref('sampleFile').child('images.jpeg');
+    link = await ref.getDownloadURL();
+    setState(() {
+      photo = Image.network('http://' + link);
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
     super.initState();
+   _getUserData();
+   _getImaage();
   }
 
   @override
@@ -48,14 +65,13 @@ class _HomePage extends State<HomePage> {
             child: Column(
               children: <Widget>[
                 SizedBox(height: 10),
-                // Likeler yukarı veya aşağı kaydırılınca kayboluyor
-                customContentFeed(userName: 'Yarkın Ata'),
+                isLoading ? CircularProgressIndicator() : customContentFeed(userName: 'Yarkın Ata', content: 'Hello world Yarkin Metin Altay'),
                 SizedBox(height: 30),
-                customContentFeed(userName: 'Yarkın Ata'),
+                //customContentFeed(userName: 'Yarkın Ata'),
                 SizedBox(height: 30),
-                customContentFeed(userName: 'Yarkın Ata'),
+                //customContentFeed(userName: 'Yarkın Ata'),
                 SizedBox(height: 30),
-                customContentFeed(userName: 'Yarkın Ata'),
+                //customContentFeed(userName: 'Yarkın Ata'),
               ],
             ),
           ),
