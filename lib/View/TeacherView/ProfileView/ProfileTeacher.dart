276a8ac600274c1daeda22761965fe483a8455d9
battletech45 <taneri862@gmail.com>
2023-04-11 22:12:23 +0300

    Updated teacher profile page

--- lib/View/TeacherView/ProfileView/ProfileTeacher.dart
+++ lib/View/TeacherView/ProfileView/ProfileTeacher.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:minerva/Control/SharedFunctions.dart';
 import 'package:minerva/Model/CustomWidgets.dart';
-
 import '../../../Model/WidgetProperties.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:minerva/Control/FirebaseFunctions.dart';
+
 
 class ProfileTeacherPage extends StatefulWidget {
   @override
@@ -9,58 +12,72 @@ class ProfileTeacherPage extends StatefulWidget {
 }
 
 class _ProfileTeacherPageState extends State<ProfileTeacherPage> {
-  //final Color mainColor = Color.fromRGBO(28, 88, 140, 1);
+
+  String email = '';
+  String userName = '';
+  String tc = '';
+  String schoolNumber = '';
+  bool isLoading = false;
+
+  _getUserData() async {
+    setState(() {
+      isLoading = true;
+    });
+    var val = await SharedFunctions.getUserEmailSharedPreference();
+    setState(() {
+      email = val!;
+    });
+    print(email);
+    var user = await FirebaseFunctions().getTeacherData(email);
+    setState(() {
+      userName = user.docs[0].get('teacherName');
+      tc = user.docs[0].get('tc');
+      schoolNumber = user.docs[0].get('teacherNumber');
+    });
+    setState(() {
+      isLoading = false;
+    });
+  }
   @override
+  void initState() {
+    super.initState();
+    _getUserData();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           elevation: 0.0,
           automaticallyImplyLeading: true,
           centerTitle: true,
-          title: Text("PROFILE",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+          title: Text("PROFILE", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
           backgroundColor: PageColors.mainColor,
         ),
-        body: Container(
-          // color: Colors.deepOrange[300],
+        body: isLoading ?
+        AnimatedSplashScreen(splash: 'assets/logo.png',splashIconSize: 200.0, disableNavigation: true, nextScreen: ProfileTeacherPage(), splashTransition: SplashTransition.fadeTransition)
+            :
+        Container(
           alignment: Alignment.center,
-          child: Column(children: [
-            SizedBox(
-              height: 20,
-            ),
+          child: Column(
+              children: <Widget>[
+            SizedBox(height: 20.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                CircleAvatar(
-                  backgroundColor: Colors.black,
-                  radius: 55,
-                ),
+                CircleAvatar(backgroundColor: Colors.black, radius: 55, child: Icon(Icons.person, size: 50.0)),
               ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Altay Taneri",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-            ),
+            SizedBox(height: 10.0),
+            Text("$userName", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
             SizedBox(
               height: 8,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "11-A",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                ),
-                SizedBox(
-                  width: 15,
-                ),
-                Text(
-                  "3231241",
-                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
-                ),
+              children: <Widget>[
+                Text("11-A", style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400)),
+                SizedBox(width: 15.0),
+                Text("$schoolNumber", style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400)),
               ],
             ),
             SizedBox(
@@ -76,92 +93,77 @@ class _ProfileTeacherPageState extends State<ProfileTeacherPage> {
                       child: Container(
                         height: 65,
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Icon(Icons.person_2),
-                            Text(
-                              "Name Surname",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w500),
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(Icons.person_2),
+                                SizedBox(width: 20.0),
+                                Text("Name Surname", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                              ],
                             ),
-                            SizedBox(
-                              width: 14,
-                            ),
-                            Text(
-                              "Altay Taneri",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.w400),
-                            ),
+                            Text("$userName", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
                           ],
                         ),
                       ),
                     ),
                     Card(
                         margin:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+                        EdgeInsets.symmetric(horizontal: 18, vertical: 5),
                         child: Container(
                           height: 65,
                           child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Icon(Icons.numbers_rounded),
-                              Text(
-                                "Teacher Number",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Icon(Icons.numbers_rounded),
+                                  SizedBox(width: 20.0),
+                                  Text("Teacher Number", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
+                                ],
                               ),
-                              SizedBox(
-                                width: 14,
+                              Text("$schoolNumber", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
+                            ],
+                          ),
+                        )),
+                    Card(
+                        margin:
+                        EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+                        child: Container(
+                          height: 65,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Icon(Icons.cabin_rounded),
+                                  SizedBox(width: 20.0),
+                                  Text("Department", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                                ],
                               ),
-                              Text(
-                                "3231241",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
+                              Text("English", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
                               ),
                             ],
                           ),
                         )),
                     Card(
                         margin:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+                        EdgeInsets.symmetric(horizontal: 18, vertical: 5),
                         child: Container(
                           height: 65,
                           child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Icon(Icons.cabin_rounded),
-                              Text(
-                                "Department",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                width: 14,
-                              ),
-                              Text(
-                                "Maths",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
-                              ),
-                            ],
-                          ),
-                        )),
-                    Card(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 5),
-                        child: Container(
-                          height: 65,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Icon(Icons.school_rounded),
-                              Text(
-                                "School",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                width: 14,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Icon(Icons.school_rounded),
+                                  SizedBox(width: 20.0),
+                                  Text("School", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                                ],
                               ),
                               Text(
                                 "Yunus Emre Anadolu Lisesi",
@@ -173,76 +175,30 @@ class _ProfileTeacherPageState extends State<ProfileTeacherPage> {
                         )),
                     Card(
                         margin:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+                        EdgeInsets.symmetric(horizontal: 18, vertical: 5),
                         child: Container(
                           height: 65,
                           child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Icon(Icons.email_rounded),
-                              Text(
-                                "Email",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Icon(Icons.email_rounded),
+                                  SizedBox(width: 20.0),
+                                  Text("Email",style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                                ],
                               ),
                               Text(
-                                "teacher@example.com",
+                                "$email",
                                 style: TextStyle(
                                     fontSize: 16, fontWeight: FontWeight.w400),
                               ),
                             ],
                           ),
-                        )),
-                    Card(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 5),
-                        child: Container(
-                          height: 65,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Icon(Icons.password_rounded),
-                              Text(
-                                "Password",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                width: 14,
-                              ),
-                              Text(
-                                "*************",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
-                              ),
-                            ],
-                          ),
-                        )),
-                    Card(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 18, vertical: 5),
-                        child: Container(
-                          height: 65,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              Icon(Icons.password_rounded),
-                              Text(
-                                "Password",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w500),
-                              ),
-                              SizedBox(
-                                width: 14,
-                              ),
-                              Text(
-                                "*************",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w400),
-                              ),
-                            ],
-                          ),
-                        )),
+                        )
+                    ),
+
                   ]),
             ),
           ]),
