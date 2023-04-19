51be6d61510d009301b6b1aadd7cafcb7c9a867b
Altay Taneri <taneri862@gmail.com>
2023-04-19 16:40:28 +0300

    Added top border to navigationbar on studemt content page

--- lib/View/StudentView/Content/StudentCourseContentPage.dart
+++ lib/View/StudentView/Content/StudentCourseContentPage.dart
@@ -39,41 +39,46 @@ class _CourseContentState extends State<CourseContent> {
        body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
-       bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: PageColors.mainColor,
-       items:  <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Column(
-              children: <Widget>[
-                Icon(FontAwesomeIcons.arrowLeft),
-                Text('Back', style: TextStyle(fontWeight: FontWeight.bold))
-              ],
+       bottomNavigationBar: Container(
+         decoration: BoxDecoration(
+           border: Border(top: BorderSide(color: PageColors.mainColor, width: 3))
+         ),
+         child: BottomNavigationBar(
+          unselectedItemColor: PageColors.mainColor,
+         items:  <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Column(
+                children: <Widget>[
+                  Icon(FontAwesomeIcons.arrowLeft),
+                  Text('Back', style: TextStyle(fontWeight: FontWeight.bold))
+                ],
+              ),
+              label: '',
             ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Column(
-              children: <Widget>[
-                Icon(FontAwesomeIcons.listCheck),
-                Text('Assesments', style: TextStyle(fontWeight: FontWeight.bold))
-              ],
-            ),
-            label: ''
-          ),
-          BottomNavigationBarItem(
-            icon: Column(
-              children: <Widget>[
-                Icon(FontAwesomeIcons.noteSticky),
-                Text('Materials', style: TextStyle(fontWeight: FontWeight.bold))
-              ],
-            ),
+            BottomNavigationBarItem(
+              icon: Column(
+                children: <Widget>[
+                  Icon(FontAwesomeIcons.listCheck),
+                  Text('Assesments', style: TextStyle(fontWeight: FontWeight.bold))
+                ],
+              ),
               label: ''
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
+            ),
+            BottomNavigationBarItem(
+              icon: Column(
+                children: <Widget>[
+                  Icon(FontAwesomeIcons.noteSticky),
+                  Text('Materials', style: TextStyle(fontWeight: FontWeight.bold))
+                ],
+              ),
+                label: ''
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped,
     ),
+       ),
       drawer: customDrawer(),
     );
   }
