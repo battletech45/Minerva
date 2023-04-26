8794e1b0f24419191b69f5eb392b503a5396405a
Altay Taneri <taneri862@gmail.com>
2023-04-27 02:16:35 +0300

    fixed null value error

--- lib/View/HomePage.dart
+++ lib/View/HomePage.dart
@@ -56,9 +56,13 @@ class _HomePage extends State<HomePage> {
   }
   
   _getAllContent() async {
+    setState(() {
+      isLoading = true;
+    });
     var val = await FirebaseFunctions().getAllContents();
     setState(() {
       contents = val;
+      isLoading = false;
     });
   }
   
@@ -80,23 +84,7 @@ class _HomePage extends State<HomePage> {
           centerTitle: true,
           title: Text("HOME", style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
         ),
-        body: /*Center(
-          child: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 10),
-                isLoading ? CircularProgressIndicator() : customContentFeed(userName: 'Yarkın Ata', content: data),
-                SizedBox(height: 30),
-                customContentFeed(userName: 'Yarkın Ata', content: 'hello world teacher now'),
-                customContentFeed(userName: 'Metin Baybars Arslan', content: 'İzmir Ekonomi Üniversitesi’nin (İEÜ) 25 öğrencisi, deprem sonrası oluşturulan barınma alanlarına yönelik alternatif projeler geliştirmek üzere 5 gün süren atölye çalışmasında bir araya geldi. Konteyner kent başta olmak üzere depremzedeler için en iyi yaşam alanlarını oluşturmak üzerine yoğunlaşan gençler, hızlı ve düşük maliyetle uygulanabilecek fikirler geliştirdi. Dörder ve beşer kişilik gruplar halinde çalışan öğrenciler, konteyner kentlerde ortak yaşam alanlarının çoğaltılabileceğine vurgu yaptı, çocukların oyun oynayabileceği ve film izleyebileceği özel bölümlerin de yer aldığı tasarımlar hazırladı. Öğrenciler, atölye sonunda İEÜ Rektörü Prof. Dr. Murat Aşkar’ın elinden sertifikalarını aldı.'),
-                SizedBox(height: 30),
-              ],
-            ),
-          ),
-        ),*/
-        contents!.docs.isNotEmpty ?
-        ListView.builder(
+        body: !isLoading ? ListView.builder(
           physics: BouncingScrollPhysics(),
           shrinkWrap: true,
           itemCount: contents!.size,
