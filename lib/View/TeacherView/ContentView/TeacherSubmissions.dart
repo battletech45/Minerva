87c7c444d003b8773b7d6f785bb5bc032a5f800f
Metin Baybars Arslan <baybarsfb2000@hotmail.com>
2023-04-27 14:49:06 +0300

    Students submissions Lists UI design added to TeacherSubmission page. Also added the feedback option.

--- lib/View/TeacherView/ContentView/TeacherSubmissions.dart
+++ lib/View/TeacherView/ContentView/TeacherSubmissions.dart
@@ -1,4 +1,6 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../Model/WidgetProperties.dart';
 
@@ -10,18 +12,105 @@ class TeacherSubmissions extends StatefulWidget {
 }
 
 class _TeacherSubmissionsState extends State<TeacherSubmissions> {
+  List<String> _students = ['Student1', 'Student 2', 'Student 3'];
+  String _feedback = '';
+  List <String> _files = [];
+
+  void _addFile(String fileName) {
+    setState(() {
+      _files.add(fileName);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Container(
-        alignment: Alignment.center,
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/logo.png', width: 300, height: 300),
-            Text('There is no submission yet !', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0, fontStyle: FontStyle.italic, color: PageColors.thirdColor))
-          ],
+          children: _students.map((String student) {
+            return Card(
+              color: Colors.grey[100],
+              child: ExpansionTile(
+                title: Text(
+                  student,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                    color: Colors.blueGrey[800],
+                  ),
+                ),
+                children: <Widget>[
+                  ListTile(
+                    title: Text(
+                      'Homework',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        color: Colors.blueGrey[700],
+                      ),
+                    ),
+                    trailing: Icon(Icons.attach_file),
+                    onTap: (){
+                      FilePicker.platform.pickFiles().then((result) {
+                        if (result != null) {
+                          String fileName = result.files.single.name;
+                          _addFile(fileName);
+                        }
+                      });
+                    },
+                  ),
+                  ListTile(
+                    title: IconButton(
+                      icon: Icon(FontAwesomeIcons.comments),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              content: TextFormField(
+                                maxLines: null,
+                                onChanged: (value) {
+                                  setState(() {
+                                    _feedback = value;
+                                  });
+                                },
+                                decoration: InputDecoration(
+                                  labelText: 'Feedback',
+                                  hintText: 'Type your feedback here...',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: Text('Save',style: TextStyle(color: PageColors.mainColor),),
+                                  onPressed: () {
+
+                                    Navigator.of(context).pop();
+
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  _feedback.isNotEmpty
+                      ? ListTile(
+                    title: Text(
+                      'Feedback: $_feedback',
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        color: Colors.blueGrey[400],
+                      ),
+                    ),
+                  )
+                      : Container(),
+                ],
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
