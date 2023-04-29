f46a1e2f864d186ba27ce37cea8523a7a9f8c60b
Altay Taneri <taneri862@gmail.com>
2023-04-29 22:41:31 +0300

    upload submission fully functional

--- lib/View/StudentView/HomeworksView/StudentHomeWorkViewPage.dart
+++ lib/View/StudentView/HomeworksView/StudentHomeWorkViewPage.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -14,11 +18,51 @@ class _StudentHomeWorkViewPageState extends State<StudentHomeWorkViewPage > {
 
   final List<HomeworkItem> homeworkItems = [];
   int _selectedIndex = -1;
+  PlatformFile? pickedFile;
+  UploadTask? uploadTask;
   bool isHomeWorkExist = false;
+  bool isSelected = false;
+  bool isLoading = false;
+  bool isLoaded = false;
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+    });
+  }
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+    setState(() {
+      pickedFile = result.files.first;
+      isSelected = true;
+    });
+  }
+
+  Future uploadFile() async {
+    setState(() {
+      isLoading = true;
+    });
+    var email = await SharedFunctions.getUserEmailSharedPreference();
+    var student = await FirebaseFunctions().getStudentData(email!);
+    String? className = await FirebaseFunctions().findStudentsClass(student.docs[0].get('studentID'));
+    final path = '$className/submissions/${pickedFile!.name}';
+    final file = File(pickedFile!.path!);
+
+    final ref = FirebaseStorage.instance.ref().child(path);
+    setState(() {
+      uploadTask = ref.putFile(file);
+    });
+
+    final snapshot = await uploadTask!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+    print('Download Link: $urlDownload');
+
+    setState(() {
+      uploadTask = null;
+      isLoading = false;
+      isLoaded = true;
     });
   }
 
@@ -85,55 +129,73 @@ class _StudentHomeWorkViewPageState extends State<StudentHomeWorkViewPage > {
         ),
         height: 300.0,
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: <Widget>[
-            Text(homeworkItems[_selectedIndex].name, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0, fontStyle: FontStyle.italic, color: PageColors.thirdColor)),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                homeworkItems[_selectedIndex].definition,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Text(homeworkItems[_selectedIndex].name, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0, fontStyle: FontStyle.italic, color: PageColors.thirdColor)),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  homeworkItems[_selectedIndex].definition,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18.0),
+                ),
               ),
-            ),
-            Visibility(
-              visible: homeworkItems[_selectedIndex].isSubmitable,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(width: 10.0),
-                  Text('Homework Material', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0, fontStyle: FontStyle.italic, color: PageColors.thirdColor)),
-                  SizedBox(width: 15.0),
-                  Icon(FontAwesomeIcons.arrowTurnDown)
-                ],
+              Visibility(
+                visible: homeworkItems[_selectedIndex].isSubmitable,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(width: 10.0),
+                    Text('Homework Material', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0, fontStyle: FontStyle.italic, color: PageColors.thirdColor)),
+                    SizedBox(width: 15.0),
+                    Icon(FontAwesomeIcons.arrowTurnDown)
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 10.0),
-            Visibility(
-              visible: homeworkItems[_selectedIndex].isSubmitable,
-              child: Container(
-                width: 300,
-                  child: Text(homeworkItems[_selectedIndex].fileName!)
+              SizedBox(height: 10.0),
+              Visibility(
+                visible: homeworkItems[_selectedIndex].isSubmitable,
+                child: Container(
+                  width: 300,
+                    child: Text(homeworkItems[_selectedIndex].fileName!)
+                ),
               ),
-            ),
-           SizedBox(height: 20.0),
-            Visibility(
-              visible: homeworkItems[_selectedIndex].isSubmitable,
-              child: MaterialButton(
-                height: 45,
-                minWidth: 45,
-                onPressed: () {
-
-                },
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                child: Text("Submit",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20
-                ),),
-                color: PageColors.thirdColor,),
-            )
-          ],
+             SizedBox(height: 20.0),
+              if(isSelected)
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+                  child: Container(
+                    height: 65,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(FontAwesomeIcons.file, size: 30.0),
+                        SizedBox(width: 20.0),
+                        Expanded(child: Text(pickedFile!.name, style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500))),
+                      ],
+                    ),
+                  ),
+                ),
+              Visibility(
+                visible: homeworkItems[_selectedIndex].isSubmitable,
+                child: MaterialButton(
+                  height: 45,
+                  minWidth: 45,
+                  onPressed: () async {
+                    await selectFile();
+                    await uploadFile();
+                  },
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  child: Text(isSelected ? isLoading ? 'Uploading' : isLoaded ? 'Uploaded' : 'Upload File' : "Choose File",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20
+                  ),),
+                  color: PageColors.thirdColor,),
+              )
+            ],
+          ),
         ),
       ) : null,
     );
