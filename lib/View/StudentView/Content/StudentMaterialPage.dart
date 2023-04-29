b81db828ea92bb23f468060dc1c5bca83b4fc44c
Altay Taneri <taneri862@gmail.com>
2023-04-29 17:30:13 +0300

    Files become displayable

--- lib/View/StudentView/Content/StudentMaterialPage.dart
+++ lib/View/StudentView/Content/StudentMaterialPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:minerva/Control/FirebaseFunctions.dart';
 import 'package:minerva/Control/SharedFunctions.dart';
 
@@ -23,6 +24,16 @@ class _StudentMaterialPageState extends State<StudentMaterialPage> {
     data.forEach((element) {
       materialURLs.add(element['materialURL']);
     });
+    if(materialURLs.isEmpty) {
+      setState(() {
+        isMaterialExist = false;
+      });
+    }
+    else {
+      setState(() {
+        isMaterialExist = true;
+      });
+    }
   }
 
   @override
@@ -33,10 +44,33 @@ class _StudentMaterialPageState extends State<StudentMaterialPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child:
-        Column(
+      body: SingleChildScrollView(
+        child: isMaterialExist ? ListView.builder(
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            itemCount: materialURLs.length,
+            itemBuilder: (context, index) {
+              if (materialURLs[index].isNotEmpty) {
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+                  child: Container(
+                    height: 65,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(FontAwesomeIcons.file, size: 30.0),
+                        SizedBox(width: 20.0),
+                        Text("Name Surname", style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500)),
+                      ],
+                    ),
+                  ),
+                );
+              }
+              return SizedBox(height: 0.0);
+            }
+        )
+        : Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.asset('assets/logo.png', width: 300, height: 300),
